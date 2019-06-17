856682880ccfec3a2c0772d80a7a3ec924b8d805
aidensymes <hello@aidensymes.com>
2019-06-17 17:25:48 -0400

    make waves responsive

--- assets/waves.js
+++ assets/waves.js
@@ -1,30 +1,45 @@
-let yoff = 0.0;
-let ylower = 0;
-let yupper = 25;
-let xlower = 0;
-let xupper = 30;
+let yoff;          // Y value for perlin noise
+let ylower = 0;    // Lower limit of Y noise
+let yupper;        // Upper limit of y noise
+let xlower = 0;    // Lower limit of X noise
+let xupper;        // Upper limit of X noise
+var xspacing = 75; // Distance between each horizontal location
+var w;             // Width of entire wave
+var theta;         // Start angle at 0
+var amplitude;     // Height of wave
+var period;        // How many pixels before the wave repeats
+var dx;            // Value for incrementing x
+var yvalues;       // Array of Y values
+var numLines;
 
-var xspacing = 50;                            // Distance between each horizontal location
-var w;                                        // Width of entire wave
-var theta = 0.0;                              // Start angle at 0
-var amplitude = 25;                           // Height of wave
-var period = ($(window).width() + 100) * .9;  // How many pixels before the wave repeats
-var dx;                                       // Value for incrementing x
-var yvalues;                                  // Array of Y values
+$(document).ready(function() {
+  $(window).resize(function() {
+    setup();
+  });
+});
 
 function setup() {
-  canvas = createCanvas($(window).width() + 100, $(window).height() * .9);
+  canvas = createCanvas($(window).width() * 1.1, $(window).height() * .75);
   canvas.parent('waves__canvas');
+
+  yoff = 0;
+  yupper = $(window).width() / 25;
+  xupper = $(window).width() / 25;
   w = width + xspacing;
+  theta = 0;
+  amplitude = $(window).width() / 65;
+  period = $(window).width();
   dx = (TWO_PI / period) * xspacing;
   yvalues = new Array(floor(w / xspacing));
+  numLines = $(window).height() / 125;
+
+  stroke('#134E6C');
+  strokeWeight(floor(amplitude / 1.5));
+  noFill();
 }
 
 function draw() {
   background('#FEFEFF');
-  stroke('#134E6C');
-  strokeWeight(amplitude / 2);
-  noFill();
   render();
 }
 
@@ -32,17 +47,17 @@ function render() {
   let xoff = 0;
   theta += map(noise(xoff, yoff), 0, 1, -0.02, 0.02);
   var x = theta;
-  for(var j = 0; j < 7; j++) {
+  for(var j = 0; j < numLines; j++) {
     beginShape();
     for (var i = 0; i < yvalues.length; i++) {
       let ynoise = map(noise(xoff, yoff), 0, 1, ylower, yupper);
       let xnoise = map(noise(xoff, yoff), 0, 1, xlower, xupper);
       yvalues[i] = sin(x) * amplitude + ynoise;
-      vertex(i * xspacing - xnoise, height - 100 - j * 1.5 * amplitude + yvalues[i]);
+      vertex(i * xspacing - xnoise, height - ($(window).width() / 20) - j * (amplitude * 1.75) + yvalues[i]);
       xoff += 0.05;
       x += dx;
     }
-    yoff += 0.001;
+    yoff += 0.0003;
     endShape();
   }
 }
